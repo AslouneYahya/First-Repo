22d179fce7b9928b813c30a83573061d9a1149f0
Yahya Asloune <aslouneyahya@gmail.com>
2024-11-19 18:54:29 +0100

    modified java file

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
-        // Creates a reader instance which takes
+        // Creates a  reader instance which takes
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
